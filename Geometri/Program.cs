09b76875501766d7b96a3b4337412affa4637c1d
Math842g <math842g@zbc.dk>
2021-08-30 12:27:42 +0200

    Add project files.

---
+++ Geometri/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Opgave A
+            Square a = new Square(4);
+            double omkreds = a.Omkreds();
+            Console.WriteLine(omkreds);
+
+            Square firkant = new Square(5);
+            double areal = firkant.Areal();
+            Console.WriteLine(areal);
+            //Opgave B
+            a.Side = 10;
+            omkreds = a.Omkreds();
+            areal = a.Areal();
+            Console.WriteLine(omkreds);
+            Console.WriteLine(areal);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Geometri/Square.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri
+{
+    class Square
+    {
+        //Opgave A
+        private double a;
+        
+        public Square(double a)
+        {
+            this.a = a;
+        }
+        public double Omkreds()
+        {
+            double omkreds = 4 * a;
+            return omkreds;
+        }
+        public double Areal()
+        {
+            double areal = Math.Pow(a, 2);
+            return areal;
+        }
+        //Opgave B
+        public double A
+        {
+            get
+            {
+                return a;
+            }
+            set
+            {
+                a = value;
+            }
+        }
+        
+
+
+    }
+}
